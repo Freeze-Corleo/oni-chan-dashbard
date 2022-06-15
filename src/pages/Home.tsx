6041c58b341574041ef437263fdd505085c4d8b9
EstebanLcn <44468643+EstebanLcn@users.noreply.github.com>
2022-06-15 13:36:12 +0200

    Close image with text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -76,8 +76,8 @@ const Home = () => {
         <div className='text-center mt-20 mb-20 text-3xl font-bold'>
           <p>TRAVAILLER AVEC ONI-CHAN</p>
         </div>
-        <div className='grid grid-cols-2 gap-0 place-items-center mb-40'>
-          <img className='object-scale-down h-60 w-60 mb-4' src={process.env.PUBLIC_URL + '/img/camion.png'} />
+        <div className='grid grid-cols-2 gap-0 place-items-center mb-40 p-10'>
+          <img className='object-scale-down h-48 w-48 mb-4 ml-32' src={process.env.PUBLIC_URL + '/img/camion.png'} />
           <div>
             <p className='mt-6 font-bold text-center text-3xl'>Livrer pour nous</p>
             <p className='mt-6 text-center'>Roulez avec Deliveroo et travaillez à votre propre rythme. En rejoignant la communauté de livreurs et livreuses partenaires de Deliveroo, vous profiterez également de tout un tas d'avantages et de réductions.</p>
@@ -86,14 +86,14 @@ const Home = () => {
             <p className='mt-6 font-bold text-center text-3xl'>Devenez partenaire</p>
             <p className='mt-6 text-center'>Devenez partenaire de Deliveroo et attirez encore plus de clients. Nous gérons la livraison, vous ne gérez que la préparation !</p>
           </div>
-          <img className='object-scale-down h-60 w-60 mb-4' src={process.env.PUBLIC_URL + '/img/serveur.png'} />
-          <img className='object-scale-down h-60 w-60 mb-4' src={process.env.PUBLIC_URL + '/img/travail.png'} />
+          <img className='object-scale-down h-48 w-48 mb-4 mr-32' src={process.env.PUBLIC_URL + '/img/serveur.png'} />
+          <img className='object-scale-down h-48 w-48 mb-4 ml-32' src={process.env.PUBLIC_URL + '/img/travail.png'} />
           <div>
             <p className='mt-6 font-bold text-center text-3xl'>Carrières</p>
             <p className='mt-6 text-center'>Notre mission est de révolutionner la manière dont vous commandez vos plats. C'est un projet ambitieux, comme nous. Et nous avons besoin de vous pour nous aider !</p>
           </div>
         </div>
-        <div className='text-center mt-20 mb-20 text-5xl font-bold'>
+        <div className='text-center mb-20 text-5xl font-bold'>
           <p>Foire aux questions</p>
         </div>
         <div className='grid grid-cols-1 place-items-center space-y-4'>
