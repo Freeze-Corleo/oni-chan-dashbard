08282f9b94e946f4a7f290c52fc8a20d83c93e36
LeoDELPON <delponleo@gmail.com>
2022-06-30 22:59:31 +0200

    Implement socket

--- src/pages/Livreur.tsx
+++ src/pages/Livreur.tsx
@@ -4,16 +4,38 @@ import Footer from '../components/organisms/Footer';
 import Navigation from '../components/organisms/Navigation';
 import { retrieveCommandsInformations } from '../core-logic/usecases/command/commandUseCase';
 import { selectCommandReducer } from '../view-model-generation/generateCommandModel';
+import io from 'socket.io-client';
+import { StringLiteralLike } from 'typescript';
+
+const ENDPOINT = 'http://localhost:6969/';
+const socket = io('ws://localhost:6969');
 
 const Livreur = () => {
   const [response, setResponse] = useState('');
   const dispatch = useDispatch();
   const commands = useSelector(selectCommandReducer);
+  const [commandId, setCommandId] = useState('');
   const commandsData: any = commands;
 
   useEffect(() => {
-    dispatch(retrieveCommandsInformations())
-  }, [])
+    dispatch(retrieveCommandsInformations());
+  }, []);
+
+  useEffect(() => {
+    const socket = io('ws://localhost:6969');
+
+    socket.on('FromAPI', (data) => {
+      setResponse(data);
+    });
+  }, [setResponse]);
+
+  const commandAccepted = (_uuid: string) => {
+    socket.emit('CommandSocket', `50% ${_uuid}`);
+  };
+
+  const deliveryManAtHome = (_uuid: string) => {
+    socket.emit('CommandSocket', `75% ${_uuid}`);
+  };
 
   return (
     <>
@@ -21,30 +43,36 @@ const Livreur = () => {
 
       {commandsData?.commands?.data ? (
         <>
-          <div className="col-span-12 bg-white rounded-lg shadow-lg">
-            <div className="grid grid-cols-3 gap-4 m-20 p-10">
-              
-                {console.log(commandsData)}
-                {commandsData?.commands?.data?.map(((command: any) => {
-                  return <>
-                  <p>Prix de la commande : {command.price} € dans la ville de {command.city}</p>
-                  <button
-                  className="px-4 py-2 bg-black text-white rounded-full"
-                >
-                  Accepter la commande
-                </button>
-                <button
-                  className="px-4 py-2 bg-black text-white rounded-full"
-                >
-                  Refuser la commande
-                </button>
-                </>
-                }))}            
+          <div className="relative z-50 col-span-12 bg-white rounded-lg shadow-lg">
+            <div className="grid grid-cols-3 gap-4 p-10 m-20">
+              <div>{response}</div>
+              {commandsData?.commands?.data?.map((command: any) => {
+                return (
+                  <>
+                    <p>
+                      Prix de la commande : {command.price} € dans la ville de{' '}
+                      {command.city}
+                    </p>
+                    <button
+                      onClick={() => {
+                        deliveryManAtHome(command.uuid);
+                      }}
+                      className="px-4 py-2 text-white bg-black rounded-full cursor-pointer"
+                    >
+                      Accepter la commande
+                    </button>
+                    <button className="px-4 py-2 text-white bg-black rounded-full cursor-pointer">
+                      Refuser la commande
+                    </button>
+                  </>
+                );
+              })}
             </div>
           </div>
         </>
-      ) : (<></>)}
-
+      ) : (
+        <></>
+      )}
 
       <Footer />
     </>
