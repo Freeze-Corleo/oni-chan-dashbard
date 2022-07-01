2546136f6c5f282101f124dae88f7cd7cce704af
PierroCesi <pierre.forques@viacesi.fr>
2022-07-01 03:07:17 +0200

    small feature redirect /feed

--- src/components/atoms/Card.tsx
+++ src/components/atoms/Card.tsx
@@ -1,14 +1,16 @@
 const Card = ({
     img = '',
     label = '',
+    link = '#',
 }: {
     img: string;
     label?: string;
+    link?: string;
 }) => {
     return (
         <>
             <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 relative">
-                <a href="#">
+                <a href={link}>
                     <img className="rounded-t-lg" src={process.env.PUBLIC_URL +  img } alt="" />
                     <div className="absolute w-full h-full top-0 text-white text-2xl flex justify-center items-center">{label}</div>
                 </a>
--- src/components/molecules/Categories.tsx
+++ src/components/molecules/Categories.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Card from '../atoms/Card';
 
 const Categories = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className='text-center mt-20 mb-20 text-4xl font-bold'>
         <p>Explorer par catégories</p>
       </div>
       <div className='grid grid-cols-3 gap-x-0 gap-y-8 mb-28 place-items-center'>
-        <Card img='/img/burger.jpg' label='EQUILIBRÉ' />
-        <Card img='/img/pizza.jpg' label='PETITS PLATS' />
-        <Card img='/img/burger.jpg' label='EQUILIBRÉ' />
-        <Card img='/img/pizza.jpg' label='PETITS PLATS' />
-        <Card img='/img/burger.jpg' label='EQUILIBRÉ' />
-        <Card img='/img/pizza.jpg' label='PETITS PLATS'/>
+        <Card img='/img/burger.jpg' label='EQUILIBRÉ' link='/feed'/>
+        <Card img='/img/pizza.jpg' label='PETITS PLATS' link='/feed'/>
+        <Card img='/img/burger.jpg' label='EQUILIBRÉ' link='/feed'/>
+        <Card img='/img/pizza.jpg' label='PETITS PLATS' link='/feed'/>
+        <Card img='/img/burger.jpg' label='EQUILIBRÉ' link='/feed'/>
+        <Card img='/img/pizza.jpg' label='PETITS PLATS' link='/feed'/>
       </div>
     </>
   );
